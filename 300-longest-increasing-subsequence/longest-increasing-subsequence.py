9da862d63cde299a2e90762491a37f12a78da22c
Adithya K G <adithyakga@gmail.com>
2026-08-26 23:47:12 +0530

    Time: 15 ms (75.12%) | Memory: 12.5 MB (97.75%) - LeetSync

---
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        arr = []
+        for num in nums:
+            left = 0
+            right = len(arr)
+
+            while left < right:
+                mid = (left + right) // 2
+
+                if arr[mid] < num:
+                    left = mid + 1
+                else:
+                    right = mid
+            if left == len(arr):
+                arr.append(num)
+            else:
+                arr[left] = num
+        return len(arr)
+
+        
