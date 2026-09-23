db9005ef78bbed865d0ef6922f4a11c821ce1e4b
Adithya K G <adithyakga@gmail.com>
2026-09-23 12:13:09 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (23.30%) - LeetSync

---
+++ 136-single-number/single-number.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        n = len(nums)
+        ans = 0
+        for i in range(n):
+            ans ^= nums[i]
+        return ans
+        
